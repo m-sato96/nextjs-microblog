d28f83aa9317b8df00bf09b7a70a3dbd3afbdf33
m-sato <ssmmaagg36@gmail.com>
2024-02-11 17:06:10 +0900

    feat: Headタグ確認

--- src/pages/posts/firstPost.tsx
+++ src/pages/posts/firstPost.tsx
@@ -1,13 +1,15 @@
+import Head from "next/head";
 import Link from "next/link";
 
 const firstPost = () => {
   return (
-    <div>
+    <>
+      <Head>最初の投稿</Head>
       <h1>最初の投稿ページ</h1>
       <Link href="/">
         <h2>Homeへ戻る</h2>
       </Link>
-    </div>
+    </>
   );
 };
 
