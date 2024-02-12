2f8fc0dbc18a9819d56c122cea51e42993cb09ea
m-sato <ssmmaagg36@gmail.com>
2024-02-12 13:59:04 +0900

    feat: トップページのblog一覧作成

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,14 +8,15 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div>
+    <div className=" max-w-5xl m-auto p-10 ">
       <Head>
         <link rel="icon" href="/favicon.icon" />
         <title>NextJS Blog</title>
       </Head>
-      <header>
-        <Image src="/images/profile.png" width={100} height={100} alt="" />
-        <h1>Sato</h1>
+      <header className="flex items-center flex-col mb-4">
+        <Image className="mb-2 block rounded-full" src="/images/profile.png" width={100} height={100} alt="" />
+        <h1 className="tex-sm font-medium">Sato</h1>
+        <p>私はフロントエンドエンジニアです🍍</p>
       </header>
       <main>{children}</main>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import Layout from "@/components/Layout";
+import Link from "next/link";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +12,45 @@ export default function Home() {
       <Head>
         <title>Next.js Blog</title>
       </Head>
-      <Layout>テスト</Layout>
+      <Layout>
+        <div className="grid grid-cols-3 gap-2.5 gap-y-5">
+          <article className="w-[300px] mx-auto">
+            <Link href="/">
+              <img className="mb-2  rounded-sm" src="https://picsum.photos/300/300"></img>
+              <p>初めまして</p>
+              <small>2024/01/01</small>
+            </Link>
+          </article>
+          <article className="w-[300px] mx-auto">
+            <Link href="/">
+              <img className="mb-2  rounded-sm" src="https://picsum.photos/300/300"></img>
+              <p>Next.jsを使ってBlogを開発しました</p>
+              <small>2024/02/12</small>
+            </Link>
+          </article>
+          <article className="w-[300px] mx-auto">
+            <Link href="/">
+              <img className="mb-2  rounded-sm" src="https://picsum.photos/300/300"></img>
+              <p>転職しました。</p>
+              <small>2024/02/19</small>
+            </Link>
+          </article>
+          <article className="w-[300px] mx-auto">
+            <Link href="/">
+              <img className="mb-2  rounded-sm" src="https://picsum.photos/300/300"></img>
+              <p>OpenAPIを使用してインタラクティブなAPI定義を作成する方法</p>
+              <small>2024/02/19</small>
+            </Link>
+          </article>
+          <article className="w-[300px] mx-auto">
+            <Link href="/">
+              <img className="mb-2  rounded-sm" src="https://picsum.photos/300/300"></img>
+              <p>GithubActionsでフロント自動テスト環境を整備する手順</p>
+              <small>2024/03/01</small>
+            </Link>
+          </article>
+        </div>
+      </Layout>
     </>
   );
 }
